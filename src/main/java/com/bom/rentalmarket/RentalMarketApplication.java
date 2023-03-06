e5478dcf954407dc99037234d142bd885f643ab8
jigi <ahraeha@naver.com>
2023-03-07 01:25:02 +0900

    Spring init Test

---
+++ src/main/java/com/bom/rentalmarket/RentalMarketApplication.java
@@ -0,0 +1,12 @@
+package com.bom.rentalmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalMarketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RentalMarketApplication.class, args);
+  }
+}
